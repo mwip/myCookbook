9dca935eb796420c46d0b2a7539023c361e605fb
mwip <matthias.weigand@protonmail.com>
2019-10-23 15:10:12 +0200

    Adding first files
    In main.py the program is started.
    ui.py contains all ui classes.
    ui/ holds all UI files created with Qt creator.
    
    in ui.py all actions of the mainwindow were added

---
+++ ui.py
@@ -0,0 +1,88 @@
+import sys
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit, QAction
+from PySide2.QtCore import QFile, QObject
+
+
+class MainWindow(QMainWindow):
+ 
+    def __init__(self, ui_file, parent=None):
+        super(MainWindow, self).__init__(parent)
+        ui_file = QFile(ui_file)
+        ui_file.open(QFile.ReadOnly)
+ 
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+        ui_file.close()
+
+
+        # Actions
+        self.action_new_cb = self.window.findChild(QAction,
+                                                   "actionNewCB")
+        self.action_new_cb.triggered.connect(self.new_cookbook)
+
+        self.action_open_cb = self.window.findChild(QAction,
+                                                    "actionOpenCB")
+        self.action_open_cb.triggered.connect(self.open_cookbook)
+
+        self.action_create_dummy_CB = self.window.findchild(QAction,
+                                                            "actionCreateDummyCB")
+        self.action_create_dummy_CB.triggered.connect(self.create_dummy_cookbook)
+
+        self.action_new_recipe = self.window.findChild(QAction,
+                                                       "actionAddRecipe")
+        self.action_new_recipe.triggered.connect(self.new_recipe)
+
+        self.action_edit_recipe = self.window.findChild(QAction,
+                                                        "actionEditRecipe")
+        self.action_edit_recipe.triggered.connect(self.edit_recipe)
+
+        self.action_remove_recipe = self.window.findChild(QAction,
+                                                          "actionRemoveRecipe")
+        self.action_remove_recipe.triggered.connect(self.remove_recipe)
+
+        self.action_add_dummy_recipe = self.window.findChild(QAction,
+                                                             "actionAddDummyRecipe")
+        self.action_add_dummy_recipe.triggered.connect(self.add_dummy_recipe)
+        
+        self.action_about = self.window.findChild(QAction,
+                                                  "actionAbout")
+        self.action_about.triggered.connect(self.about)
+        
+        self.action_exit = self.window.findChild(QAction,
+                                                 "actionExit")
+        self.action_exit.triggered.connect(self.exit_app)
+
+        # show main window
+        self.window.show()
+
+
+    # Functionality
+    def new_cookbook(self):
+        pass
+
+    def open_cookbook(self):
+        pass
+
+    def create_dummy_cookbook(self):
+        pass
+
+    def new_recipe(self):
+        pass
+
+    def edit_recipe(self):
+        pass
+
+    def remove_recipe(self):
+        pass
+
+    def add_dummy_recipe(self):
+        pass
+
+    def about(self):
+        pass
+        
+
+    def exit_app(self):
+        print("exit")
+        self.window.close()
