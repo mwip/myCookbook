60f28cb93a648ecfdfe8d94a9e04e96fd4dc09bb
mwip <matthias.weigand@protonmail.com>
2019-10-23 16:09:52 +0200

    Fix "Qt WebEngine seems to be initialized from a plugin" error

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #### from https://www.blog.pythonlibrary.org/2018/05/30/loading-ui-files-in-qt-for-python/
 import sys
 from PySide2.QtWidgets import QApplication
+from PySide2.QtCore import Qt, QCoreApplication
 from ui import MainWindow
 
 if __name__ == '__main__':
+    QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QApplication(sys.argv)
     form = MainWindow('ui/mainwindow.ui')
     sys.exit(app.exec_())
